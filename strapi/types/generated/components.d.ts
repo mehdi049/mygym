0ae755296ebee54a59314ce99c0c18786486ee91
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2024-02-05 09:56:51 +0100

    update gym and user info collections

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,5 +1,50 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface AddressAddress extends Schema.Component {
+  collectionName: 'components_address_addresses';
+  info: {
+    displayName: 'address';
+  };
+  attributes: {
+    city: Attribute.String;
+    zip_code: Attribute.String;
+    street: Attribute.String;
+  };
+}
+
+export interface AddressMap extends Schema.Component {
+  collectionName: 'components_address_maps';
+  info: {
+    displayName: 'map';
+  };
+  attributes: {
+    langitude: Attribute.String;
+    latitude: Attribute.String;
+  };
+}
+
+export interface SocialMediaLinksSocialMediaLinks extends Schema.Component {
+  collectionName: 'components_social_media_links_social_media_links';
+  info: {
+    displayName: 'Social media links';
+    description: '';
+  };
+  attributes: {
+    website: Attribute.String;
+    facebook: Attribute.String;
+    instagram: Attribute.String;
+    twitter: Attribute.String;
+    youtube: Attribute.String;
+    linkedin: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'address.address': AddressAddress;
+      'address.map': AddressMap;
+      'social-media-links.social-media-links': SocialMediaLinksSocialMediaLinks;
+    }
+  }
 }
